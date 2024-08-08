574b89e31eda1022e4830dfce8aef91a831f1412
savidyainduma <87478305+savidyainduma@users.noreply.github.com>
2024-08-08 16:24:49 +0530

    done some changes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Header.css'
+import { assets } from '../../assets/assets'
 
 const Header = () => {
   return (
     <div className='header'>
+      <img src={assets.header_img} alt="" />
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes crafted with finest ingredients and culinary expertise. Our mission is satisfy your carvings and elevate your dining experience, one delicious meal at a time.</p>
